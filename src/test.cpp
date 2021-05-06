ba33e2e28c40e379e59e48ddc1e8192c84d2f8a0
QuantumForceField <quantumforcefield@gmail.com>
2021-05-07 00:43:45 +0800

    Added test cases amd Makefile

--- src/test.cpp
+++ src/test.cpp
@@ -7,28 +7,126 @@ using namespace std;
 
 template <class T>
 void test(EvolutionGNN<T>& gnn, string printout, int iterations = 10) {
-	cout << printout << endl;
+	cout << endl << printout << endl;
 	
 	for(int i = 0; i < iterations; ++i){
 		gnn.run();
 		gnn.flipBuffer();
 		cout << setw(7) << gnn.getOutput(0);
 	}
+	cout << endl;
 }
 
 int main(){
 
 	cout << "Running test..." << endl;
 	
-	cout << "Constructing a GNN..." << endl;
+	//Testing AND operation
+	EvolutionGNN<float> orGate(2, 1);
+	//Add nodes
+	orGate.addNode(3);
+	//Add connections
+	orGate.addConnection(0, 4, 20);
+	orGate.addConnection(1, 5, 20);
+	orGate.addConnection(3, 3, 20, 1, 1);
+	orGate.addConnection(3, 4, 20);
+	orGate.addConnection(3, 5, 20);
+	orGate.addConnection(3, 2, -20);
+	orGate.addConnection(4, 2, 40);
+	orGate.addConnection(5, 2, 40);
+	//Set input
+	orGate.setInput(0, -1);
+	orGate.setInput(1, -1);
+	//Test
+	test(orGate, "OR GATE with input [-1, -1], expected output [-1]");
 	
-	//The GNN will have 2 input and 1 output
-	EvolutionGNN<float> gnn1(2, 1);
+	//Set input
+	orGate.setInput(0, 1);
+	orGate.setInput(1, -1);
+	//Test
+	test(orGate, "OR GATE with input [1, -1], expected output [1]");
 	
-	//Add gate
+	//Set input
+	orGate.setInput(0, -1);
+	orGate.setInput(1, 1);
+	//Test
+	test(orGate, "OR GATE with input [-1, 1], expected output [1]");
+	
+	//Set input
+	orGate.setInput(0, 1);
+	orGate.setInput(1, 1);
+	//Test
+	test(orGate, "OR GATE with input [1, 1], expected output [1]");
 	
 	
-	test(gnn1, "And gate with input [0, 0], expected output [0]");
-
+	
+	//Testing OR operation
+	EvolutionGNN<float> andGate(2, 1);
+	//Add nodes
+	andGate.addNode(3);
+	//Add connections
+	andGate.addConnection(0, 2, 40);
+	andGate.addConnection(1, 2, 40);
+	andGate.addConnection(3, 3, 20, 1, 1);
+	andGate.addConnection(3, 2, -60);
+	//Set input
+	andGate.setInput(0, -1);
+	andGate.setInput(1, -1);
+	//Test
+	test(andGate, "AND GATE with input [-1, -1], expected output [-1]");
+	
+	//Set input
+	andGate.setInput(0, 1);
+	andGate.setInput(1, -1);
+	//Test
+	test(andGate, "AND GATE with input [1, -1], expected output [-1]");
+	
+	//Set input
+	andGate.setInput(0, -1);
+	andGate.setInput(1, 1);
+	//Test
+	test(andGate, "AND GATE with input [-1, 1], expected output [-1]");
+	
+	//Set input
+	andGate.setInput(0, 1);
+	andGate.setInput(1, 1);
+	//Test
+	test(andGate, "AND GATE with input [1, 1], expected output [1]");
+	
+	
+	//Testing to save neural network
+	cout << "Saving AND_GATE.TEvoGNN..." << endl;
+	andGate.save("AND_GATE.TEvoGNN");
+	cout << "Done." << endl;
+	
+	//Testing to load neural network
+	EvolutionGNN<float> loaded;
+	cout << "Loading from AND_GATE.TEvoGNN" << endl;
+	loaded.load("AND_GATE.TEvoGNN");
+	//Set input
+	loaded.setInput(0, -1);
+	loaded.setInput(1, -1);
+	//Test
+	test(loaded, "Loaded AND GATE with input [-1, -1], expected output [-1]");
+	
+	//Set input
+	loaded.setInput(0, 1);
+	loaded.setInput(1, -1);
+	//Test
+	test(loaded, "Loaded AND GATE with input [1, -1], expected output [-1]");
+	
+	//Set input
+	loaded.setInput(0, -1);
+	loaded.setInput(1, 1);
+	//Test
+	test(loaded, "Loaded AND GATE with input [-1, 1], expected output [-1]");
+	
+	//Set input
+	loaded.setInput(0, 1);
+	loaded.setInput(1, 1);
+	//Test
+	test(loaded, "Loaded AND GATE with input [1, 1], expected output [1]");
+	
+	
 	return 0;
 }
